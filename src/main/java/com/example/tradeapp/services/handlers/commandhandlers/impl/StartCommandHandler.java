c192b2b7313026f4aa6094be573f7ad03fc9d1a9
FilosofDanil <serhio3347@gmail.com>
2023-12-20 20:21:50 +0100

    Added TextHandler interface.

--- src/main/java/com/example/tradeapp/services/handlers/commandhandlers/impl/StartCommandHandler.java
+++ src/main/java/com/example/tradeapp/services/handlers/commandhandlers/impl/StartCommandHandler.java
@@ -5,6 +5,7 @@ import com.example.tradeapp.components.MessageSender;
 import com.example.tradeapp.entities.messages.impl.TextMessage;
 import com.example.tradeapp.entities.session.UserSession;
 import com.example.tradeapp.services.handlers.commandhandlers.CommandHandler;
+import com.example.tradeapp.services.session.SessionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -18,11 +19,15 @@ public class StartCommandHandler implements CommandHandler {
 
     private final MessageDirector messageDirector;
 
+    private final SessionService sessionService;
+
     @Override
     public void handle(UserSession session, Update update) {
         String text = "Вітаємо!";
         textMessageMessageSender.sendMessage(messageDirector
                 .buildTextMessage(update.getMessage().getChatId(), text));
+        session.setHandler("started");
+        sessionService.updateSession(update.getMessage().getChatId(), session);
     }
 
     @Override
