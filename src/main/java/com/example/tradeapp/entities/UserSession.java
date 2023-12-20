8bca1e5ac11c5e973ef855b98d6372ef198d4995
FilosofDanil <serhio3347@gmail.com>
2023-12-20 14:47:45 +0100

    Added Redis. Created UserSession entity, in order to provide efficient communication

---
+++ src/main/java/com/example/tradeapp/entities/UserSession.java
@@ -0,0 +1,21 @@
+package com.example.tradeapp.entities;
+
+import com.example.tradeapp.services.Handler;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.util.Map;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash("session")
+public class UserSession {
+    private Long id;
+    private Map<String, String> userData;
+    private Handler handler;
+}
---
+++ src/main/java/com/example/tradeapp/services/Handler.java
@@ -0,0 +1,4 @@
+package com.example.tradeapp.services;
+
+public interface Handler {
+}
