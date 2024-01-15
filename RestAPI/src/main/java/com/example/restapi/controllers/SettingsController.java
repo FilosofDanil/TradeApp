d66fdc4b510ec47abf079ec57f8dd79b038a74cc
FilosofDanil <serhio3347@gmail.com>
2024-01-15 18:45:25 +0100

    SettingsController finished

--- RestAPI/src/main/java/com/example/restapi/controllers/SettingsController.java
+++ RestAPI/src/main/java/com/example/restapi/controllers/SettingsController.java
@@ -6,12 +6,12 @@ import com.example.restapi.entites.Item;
 import com.example.restapi.entites.Settings;
 import com.example.restapi.services.crud.CRUDService;
 import com.example.restapi.services.itemservice.ItemService;
+import com.example.restapi.services.settingsservice.SettingsService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,16 +21,45 @@ import java.util.List;
 public class SettingsController {
     private final CRUDService<Settings> crudService;
 
-    private final ItemService itemService;
+    private final SettingsService settingsService;
 
     private final ModelMapper modelMapper;
 
     @GetMapping("")
-    public ResponseEntity<List<SettingsDTO>> getAllItems() {
+    public ResponseEntity<List<SettingsDTO>> getAllSettings() {
         return ResponseEntity.ok(crudService.getAll()
                 .stream()
                 .map(this::toDto)
                 .toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SettingsDTO> getSettingsById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(toDto(crudService.getById(id)));
+    }
+
+    @GetMapping("/user/{username}")
+    public ResponseEntity<SettingsDTO> getSettingsByUsername(@PathVariable("username") String username) {
+        return ResponseEntity.ok(toDto(settingsService.getByUsername(username)));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<SettingsDTO> createSettings(@RequestBody SettingsDTO settingsDTO) {
+        Settings settings = settingsService.toEntity(settingsDTO);
+        return new ResponseEntity<>(toDto(crudService.create(settings)), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<HttpStatus> updateSettings(@PathVariable("id") Long id, @RequestBody SettingsDTO settingsDTO) {
+        Settings settings = settingsService.toEntity(settingsDTO);
+        crudService.update(id, settings);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteSettings(@PathVariable("id") Long id) {
+        crudService.delete(id);
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 
     private SettingsDTO toDto(Settings settings) {
