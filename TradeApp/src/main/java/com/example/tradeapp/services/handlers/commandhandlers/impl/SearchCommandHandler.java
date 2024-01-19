9269f39d5a2daa04e1a06abefc245027f915d1eb
FilosofDanil <serhio3347@gmail.com>
2024-01-19 13:20:52 +0100

    Created Searching command handler

---
+++ TradeApp/src/main/java/com/example/tradeapp/services/handlers/commandhandlers/impl/SearchCommandHandler.java
@@ -0,0 +1,41 @@
+package com.example.tradeapp.services.handlers.commandhandlers.impl;
+
+import com.example.tradeapp.builder.director.MessageDirector;
+import com.example.tradeapp.components.ChatIdFromUpdateComponent;
+import com.example.tradeapp.components.MessageSender;
+import com.example.tradeapp.entities.messages.impl.TextMessage;
+import com.example.tradeapp.entities.session.UserSession;
+import com.example.tradeapp.services.handlers.commandhandlers.CommandHandler;
+import com.example.tradeapp.services.session.SessionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+@RequiredArgsConstructor
+public class SearchCommandHandler implements CommandHandler {
+    private final static String command = "/search";
+
+    private final MessageSender<TextMessage> textMessageMessageSender;
+
+    private final MessageDirector messageDirector;
+
+    private final SessionService sessionService;
+
+    private final ChatIdFromUpdateComponent updateComponent;
+
+    @Override
+    public void handle(UserSession session, Update update) {
+        String text = "Починаємо пошук товарів, відповідно ваших налаштувань...";
+        Long chatId = updateComponent.getChatIdFromUpdate(update);
+        textMessageMessageSender.sendMessage(messageDirector
+                .buildTextMessage(chatId, text));
+        session.setHandler("searching");
+        sessionService.updateSession(chatId, session);
+    }
+
+    @Override
+    public String getCommand() {
+        return command;
+    }
+}
